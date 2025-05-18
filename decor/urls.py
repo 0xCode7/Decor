f04e8aa502edb56ec0e07c78b3349152bb35178a
0xCode7 <mahmoueddragon2004@gmail.com>
2025-05-18 07:29:23 +0300

    fix images url in db and urls.py

--- decor/urls.py
+++ decor/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from rest_framework import routers
 from decor import settings
 from item.views import ItemViewSet, CategoryViewSet, SubCategoryViewSet
@@ -13,7 +15,6 @@ router.register('sub-categories', SubCategoryViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
